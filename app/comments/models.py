e3119899b167e98a3c669c29fc17e6580b020dfd
INSEOP KIM <inseop0813@gmail.com>
2024-03-20 14:38:14 +0900

    comments model create

--- app/comments/models.py
+++ app/comments/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from common.models import CommonModel
+from users.models import User
+from videos.models import Video
 
-# Create your models here.
+class Comment(CommonModel):
+    content = models.TextField()
+    like = models.PositiveIntegerField(default=0)
+    dislike = models.PositiveIntegerField(default=0)
+
+    # User:Comment => 1:N
+    # - User => Comment, Comment, Comment, Comment (O)
+    # - Comment => User, User, User (X)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Video:Comment(FK) => 1:N
+    # - Video => Comment, Comment, Comment => O
+    # - Comment => Video(침착맨), Video(이지금), Video(잇섭) => X
+
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    # 법(law) -> 채팅 데이터 3개월
+
+    # docker-compose run --rm app sh -c 'python manage.py makemigrations'
+    # docker-compose run --rm app sh -c 'python manage.py migrate'
