a67b7abd1170844cecb8ede41adcfa538bc7da06
INSEOP KIM <inseop0813@gmail.com>
2024-03-27 14:36:30 +0900

    docker update

--- app/app/settings.py
+++ app/app/settings.py
@@ -142,9 +142,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -163,8 +160,8 @@ CHANNEL_LAYERS = {
     }
 }
 
-STATIC_URL = '/static/static'
-MEDIA_URL = '/static/media'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
